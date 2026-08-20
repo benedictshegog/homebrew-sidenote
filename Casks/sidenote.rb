e81c370c59363fd38bf5b52bebb09a13ce41eafb
Benedict Shegog <benedict.shegog@granular-energy.com>
2026-08-20 16:34:13 +0100

    sidenote 0.1.1

--- Casks/sidenote.rb
+++ Casks/sidenote.rb
@@ -1,6 +1,6 @@
 cask "sidenote" do
-  version "0.1.0"
-  sha256 "0ab8089e8cfd7a6fe0f24f585cedd6a64837d7a916f0b7a58f7997bc15d7112d"
+  version "0.1.1"
+  sha256 ""
 
   url "https://github.com/benedictshegog/sidenote/releases/download/v#{version}/Sidenote_#{version}_aarch64.dmg"
   name "Sidenote"
